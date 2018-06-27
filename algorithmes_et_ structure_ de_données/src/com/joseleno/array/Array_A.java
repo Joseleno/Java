fbc875638f5a5088d36fefbac7e765547765939a
Joseleno <joselenomoreira@gmail.com>
2018-06-26 23:09:36 -0300

    Arrays

---
+++ algorithmes_et_ structure_ de_données/src/com/joseleno/array/Array_A.java
@@ -0,0 +1,13 @@
+package com.joseleno.array;
+
+/**
+ * @author joseleno on 26/06/18
+ */
+public class Array_A {
+    public static void main(String[] args) {
+        int[] ages = new int[10];
+        for (int i = 0; i< ages.length; i++){
+            ages[i]= i+10;
+        }
+    }
+}
---
+++ algorithmes_et_ structure_ de_données/src/com/joseleno/array/Array_B.java
@@ -0,0 +1,22 @@
+package com.joseleno.array;
+
+/**
+ * @author joseleno on 26/06/18
+ */
+public class Array_B {
+    public static void main(String[] args) {
+        int taille = 3;
+        //Numbers - double, float, long, int, short, byte
+        int[] numberExemple = new int[taille];
+        //boolean
+        boolean[] booleanExemple = new boolean[taille];
+        //Objects
+        String[] stringExemple = new String[taille];
+
+        for (int i = 0; i< taille; i++){
+            System.out.println("Numeros: " + numberExemple[i]);
+            System.out.println("Boolean: " + booleanExemple[i]);
+            System.out.println("Objects: " + stringExemple[i]);
+        }
+    }
+}
